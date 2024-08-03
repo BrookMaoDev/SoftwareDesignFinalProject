8919715250acb4a9ef1dc50c17f82759792c47db
Bill Jin <149437563+bzyjin@users.noreply.github.com>
2024-08-03 16:26:26 -0400

    Minor changes
    Removed unused imports and added null checking

--- app/src/main/java/com/example/b07demosummer2024/ItemCatalogue.java
+++ app/src/main/java/com/example/b07demosummer2024/ItemCatalogue.java
@@ -2,18 +2,14 @@ package com.example.b07demosummer2024;
 
 import androidx.annotation.NonNull;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.BiFunction;
 
 public class ItemCatalogue {
 
@@ -107,6 +103,9 @@ public class ItemCatalogue {
      * Apply the filter to the catalogue.
      */
     public void applyFilter() {
+        if (this.filter == null) {
+            return;
+        }
         this.filter.applyToList(this.items);
     }
 
